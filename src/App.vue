3964d46df53e6d6fe995ef5b1f5795be76e69bca
瞐法师 <53597750+jingjingxiaoxiannv@users.noreply.github.com>
2024-07-29 14:14:37 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Author: zhoukai zk3366386@163.com
+ * @Date: 2023-08-24 17:24:18
+ * @LastEditors: zhoukai zk3366386@163.com
+ * @LastEditTime: 2024-02-05 11:52:13
+ * @FilePath: \h5newxunjian\public\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  created() {
+   
+  }
+};
+</script>
+<style lang="less">
+* {
+  padding: 0;
+  margin: 0;
+}
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+  font-size: 18px;
+  // background: #00d6c1;
+  // color: white;
+}
+</style>
